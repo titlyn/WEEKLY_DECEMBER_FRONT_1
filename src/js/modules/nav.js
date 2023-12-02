a7f1aae5979a0e89680fead49597ff86b681280d
Tolotra Mandresy <tolotramandresy39@gmail.com>
2023-12-02 12:47:41 +0300

    navbar responsive

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -1,11 +1,30 @@
-/**
- * 
- * @param {HTMLAnchorElement[]} links 
- */
-export function setUpNavLinkClick(links){
-    links.forEach( link => {
+import { NAV_MOBIL_WRAPPER, NAV_LINKS, NAV_TOGGLER } from './elements.js'
+
+export function setUpNavActions(){
+    NAV_LINKS.forEach( link => {
         link.addEventListener('click', function(){
             toggleActiveLink(link)
         }) 
     })
+
+    NAV_TOGGLER.onclick = toggleMobilNav
 }
+
+function toggleMobilNav(){
+    NAV_MOBIL_WRAPPER.classList.toggle('visible')
+}
+
+const toggleActiveLink = (function(){
+    let lastActiveLink = [...NAV_LINKS].find( link => link.classList.contains('active'))
+
+    /**
+     * 
+     * @param {HTMLAnchorElement} newActiveLink 
+     */
+    return function(newActiveLink){
+        lastActiveLink.classList.remove('active')
+        newActiveLink.classList.add('active')
+
+        lastActiveLink= newActiveLink
+    }
+})()
