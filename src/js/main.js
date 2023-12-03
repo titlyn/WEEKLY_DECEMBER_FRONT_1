b7ddf31d7f8d78d0ef78933bf3333b8e0f5831a9
Tolotra Mandresy <tolotramandresy39@gmail.com>
2023-12-03 22:27:56 +0300

    scroll spy implemented

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import {setUpNavActions} from './modules/nav.js';
 import {onLoaded} from './modules/loader.js'
+import {setUpScrollSpy} from './modules/scrollspy.js'
 
 setUpNavActions()
 document.addEventListener('readystatechange', (e)=>{
     // if(document.readyState == 'loading') 
-    if(document.readyState == 'complete') onLoaded()
+    if(document.readyState == 'complete') {
+        onLoaded()
+        setUpScrollSpy()
+    }
 })
